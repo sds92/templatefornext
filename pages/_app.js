0aefe6c3407508f05d4011d0e8f3f18aca95f870
sds92 <safronovds92@yandex.ru>
2022-01-13 15:26:02 +0300

    stable

--- pages/_app.js
+++ pages/_app.js
@@ -24,7 +24,7 @@ function MyApp({ Component, pageProps }) {
       },
       []
     );
-    document.onload = function () {
+    window.onload = function () {
         alert('111')
     }
     document.onreadystatechange = function () {
