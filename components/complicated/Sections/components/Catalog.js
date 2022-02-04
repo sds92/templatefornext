c99077764dedc0e22fd5db05e1c328f072e501ad
sds92 <safronovds92@yandex.ru>
2022-02-04 14:25:27 +0300

    stable

--- components/complicated/Sections/components/Catalog.js
+++ components/complicated/Sections/components/Catalog.js
@@ -12,8 +12,10 @@ import '@szhsin/react-menu/dist/index.css';
 import { Icons } from '../../';
 
 export default function Catalog({ w, lgView, content, app, theme, products }) {
+  console.log("🚀 ~ file: Catalog.js ~ line 15 ~ Catalog ~ products", products)
+  // let productId = null
   const [state, setState] = React.useState({
-    chosen: 0,
+    chosen: products.map((item, i) => ({prTitle: item.title, num: i})).find(({prTitle}) => prTitle === 'Top').num,
     hover: null,
     show: false,
   });
