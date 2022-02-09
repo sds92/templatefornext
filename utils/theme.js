a869f8e2f7af2fcd5a941ed0ac633e40deee7be7
sds92 <safronovds92@yandex.ru>
2022-02-09 18:43:38 +0300

    stable

--- utils/theme.js
+++ utils/theme.js
@@ -6,8 +6,9 @@ const theme = (name) => {
       theme.text = 'text-white';
       theme.textDark = 'text-zinc-800';
       theme.hoverText = 'text-belplit24_2';
-      theme.buttonColours = 'bg-belplit24_2 text-white hover:bg-belplit24_2_b hover:border-belplit24_2_b rounded-md';
-      theme.button = 'bg-belplit24_2 text-white hover:bg-belplit24_2_b hover:border-belplit24_2_b rounded-md'
+      theme.buttonColours =
+        'bg-belplit24_2 text-white hover:bg-belplit24_2_b hover:border-belplit24_2_b rounded-md';
+      theme.button = 'bg-belplit24_2 text-white hover:bg-belplit24_2_b hover:border-belplit24_2_b rounded-md';
       break;
     case 'blue':
       theme.bg = 'bg-slate-500';
@@ -15,7 +16,7 @@ const theme = (name) => {
       theme.textDark = 'text-blue-800';
       theme.hoverText = 'text-blue-900';
       theme.buttonColours = 'bg-amber-800 text-white';
-      theme.button = 'bg-belplit24_2 text-white hover:bg-belplit24_2_b hover:border-belplit24_2_b rounded-md'
+      theme.button = 'bg-belplit24_2 text-white hover:bg-belplit24_2_b hover:border-belplit24_2_b rounded-md';
       break;
     case 'yellow':
       theme.bg = 'bg-amber-800';
@@ -23,7 +24,17 @@ const theme = (name) => {
       theme.textDark = 'text-yellow-400';
       theme.hoverText = 'text-amber-300';
       theme.buttonColours = 'bg-amber-800 text-white';
-      theme.button = 'bg-belplit24_2 text-white hover:bg-belplit24_2_b hover:border-belplit24_2_b rounded-md'
+      theme.button = 'bg-belplit24_2 text-white hover:bg-belplit24_2_b hover:border-belplit24_2_b rounded-md';
+      break;
+    case 'green':
+      theme.bg = {
+        headfoot: 'bg-zinc-800',
+      };
+      theme.text = 'text-zinc-100';
+      theme.textDark = 'text-belplit-400';
+      theme.hoverText = 'text-amber-300';
+      theme.buttonColours = 'bg-amber-800 text-white';
+      theme.button = 'bg-belplit24_2 text-white hover:bg-belplit24_2_b hover:border-belplit24_2_b rounded-md';
       break;
     default:
       break;
