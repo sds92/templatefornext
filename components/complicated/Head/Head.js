3ce50c17151b9eda90e06eef231c05a56d81e226
Dmitry <safronovds92@yandex.ru>
2022-02-16 18:39:49 +0300

    stable

--- components/complicated/Head/Head.js
+++ components/complicated/Head/Head.js
@@ -1,6 +1,5 @@
 import React from 'react';
 import Head from 'next/head';
-import Script from 'next/script';
 
 export default function HeadComponent({ head, children }) {
   return (
@@ -17,7 +16,23 @@ export default function HeadComponent({ head, children }) {
         rel='stylesheet'
       ></link>
       {children && children}
-      
+      <script
+            dangerouslySetInnerHTML={{
+              __html: `
+        (function(m,e,t,r,i,k,a){m[i]=m[i]function(){(m[i].a=m[i].a[]).push(arguments)};
+        m[i].l=1*new Date();k=e.createElement(t),a=e.getElementsByTagName(t)[0],k.async=1,k.src=r,a.parentNode.insertBefore(k,a)})
+        (window, document, "script", "https://cdn.jsdelivr.net/npm/yandex-metrica-watch/tag.js", "ym");
+        
+        ym(${app[2].api.ym}, "init", {
+             clickmap:true,
+             trackLinks:true,
+             accurateTrackBounce:true,
+             webvisor:true,
+             trackHash:true
+        });
+`,
+            }}
+          />
     </Head>
   );
 }
--- pages/_app.js
+++ pages/_app.js
@@ -53,23 +53,7 @@ function MyApp({ Component, pageProps }) {
             <Component {...newProps} />
             <Footer app={newProps.app} theme={newProps.theme} />
           </div>
-          <script
-            dangerouslySetInnerHTML={{
-              __html: `
-        (function(m,e,t,r,i,k,a){m[i]=m[i]function(){(m[i].a=m[i].a[]).push(arguments)};
-        m[i].l=1*new Date();k=e.createElement(t),a=e.getElementsByTagName(t)[0],k.async=1,k.src=r,a.parentNode.insertBefore(k,a)})
-        (window, document, "script", "https://cdn.jsdelivr.net/npm/yandex-metrica-watch/tag.js", "ym");
-        
-        ym(${app[2].api.ym}, "init", {
-             clickmap:true,
-             trackLinks:true,
-             accurateTrackBounce:true,
-             webvisor:true,
-             trackHash:true
-        });
-`,
-            }}
-          />
+          
         </>
       )}
     </>
