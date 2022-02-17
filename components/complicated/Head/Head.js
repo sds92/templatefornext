6d17aaf56c37150d8aa8b2674fce1ce5035df22a
Dmitry <safronovds92@yandex.ru>
2022-02-17 10:56:12 +0300

    stable

--- components/complicated/Head/Head.js
+++ components/complicated/Head/Head.js
@@ -14,7 +14,6 @@ export default function HeadComponent({ head, children, ...props }) {
         dangerouslySetInnerHTML={{
           __html: `
           <!-- Yandex.Metrika counter -->
-          <script type="text/javascript" >
              (function(m,e,t,r,i,k,a){m[i]=m[i]||function(){(m[i].a=m[i].a||[]).push(arguments)};
              m[i].l=1*new Date();k=e.createElement(t),a=e.getElementsByTagName(t)[0],k.async=1,k.src=r,a.parentNode.insertBefore(k,a)})
              (window, document, "script", "https://cdn.jsdelivr.net/npm/yandex-metrica-watch/tag.js", "ym");
@@ -26,7 +25,6 @@ export default function HeadComponent({ head, children, ...props }) {
                   webvisor:true,
                   trackHash:true
              });
-          </script>
           <!-- /Yandex.Metrika counter -->
   `,
         }}
