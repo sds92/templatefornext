33b6b670fdf360c70966397652d54863ea5db1aa
sds92 <safronovds92@yandex.ru>
2022-02-18 11:43:32 +0300

    stable

--- pages/_app.js
+++ pages/_app.js
@@ -49,10 +49,12 @@ function MyApp({ Component, pageProps }) {
           <Head head={newProps.input.head}></Head>
           <Component {...newProps} />
           <Footer app={newProps.app} />
-          <script
-            type='text/javascript'
-            dangerouslySetInnerHTML={{
-              __html: `
+        </div>
+      )}
+      <script
+        type='text/javascript'
+        dangerouslySetInnerHTML={{
+          __html: `
              (function(m,e,t,r,i,k,a){m[i]=m[i]||function(){(m[i].a=m[i].a||[]).push(arguments)};
              m[i].l=1*new Date();k=e.createElement(t),a=e.getElementsByTagName(t)[0],k.async=1,k.src=r,a.parentNode.insertBefore(k,a)})
              (window, document, "script", "https://cdn.jsdelivr.net/npm/yandex-metrica-watch/tag.js", "ym");
@@ -65,10 +67,8 @@ function MyApp({ Component, pageProps }) {
                   trackHash:true
              });
   `,
-            }}
-          />
-        </div>
-      )}
+        }}
+      />
     </>
   );
 }
