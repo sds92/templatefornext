959adcd4cba95129cfc13be4d4643e715e748b6b
sds92 <safronovds92@yandex.ru>
2022-02-18 11:47:20 +0300

    stable

--- pages/_app.js
+++ pages/_app.js
@@ -63,26 +63,26 @@ function MyApp({ Component, pageProps }) {
           <Head head={newProps.input.head}></Head>
           <Component {...newProps} />
           <Footer app={newProps.app} />
-          <script
-            type='text/javascript'
-            dangerouslySetInnerHTML={{
-              __html: `
-             (function(m,e,t,r,i,k,a){m[i]=m[i]||function(){(m[i].a=m[i].a||[]).push(arguments)};
-             m[i].l=1*new Date();k=e.createElement(t),a=e.getElementsByTagName(t)[0],k.async=1,k.src=r,a.parentNode.insertBefore(k,a)})
-             (window, document, "script", "https://cdn.jsdelivr.net/npm/yandex-metrica-watch/tag.js", "ym");
-          
-             ym(${newProps.ymNum}, "init", {
-                  clickmap:true,
-                  trackLinks:true,
-                  accurateTrackBounce:true,
-                  webvisor:true,
-                  trackHash:true
-             });
-  `,
-            }}
-          />
         </div>
       )}
+      <script
+        type='text/javascript'
+        dangerouslySetInnerHTML={{
+          __html: `
+         (function(m,e,t,r,i,k,a){m[i]=m[i]||function(){(m[i].a=m[i].a||[]).push(arguments)};
+         m[i].l=1*new Date();k=e.createElement(t),a=e.getElementsByTagName(t)[0],k.async=1,k.src=r,a.parentNode.insertBefore(k,a)})
+         (window, document, "script", "https://cdn.jsdelivr.net/npm/yandex-metrica-watch/tag.js", "ym");
+      
+         ym(${newProps.ymNum}, "init", {
+              clickmap:true,
+              trackLinks:true,
+              accurateTrackBounce:true,
+              webvisor:true,
+              trackHash:true
+         });
+`,
+        }}
+      />
     </>
   );
 }
