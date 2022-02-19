955b0f6d03a46e2222cc845fc27c9bd3482f05b2
Dmitry <safronovds92@yandex.ru>
2022-02-19 19:09:47 +0300

    stable

--- utils/functions.js
+++ utils/functions.js
@@ -1,10 +1,14 @@
 export const plitaosb3ru = (inputArr) => {
-  // console.log('🚀 ~ file: functions.js ~ line 2 ~ plitaosb3ru ~ inputArr', inputArr);
   let arr = [];
   return inputArr
     .map((item) => ({
-      title: item.options.find(({ key }) => key === 'Коллекция').value,
+      title: item.options.find(({ key }) => key === 'Производитель').value,
       infos: item.title,
+      // sizes: {
+      //   a: parseInt(item.options.find(({ key }) => key === 'Длина').value.replace(' мм', '')),
+      //   b: parseInt(item.options.find(({ key }) => key === 'Ширина').value.replace(' мм', '')),
+      //   h: parseInt(item.options.find(({ key }) => key === 'Толщина').value.replace(' мм', '')),
+      // },
       prices: item.cost,
       priceFor: item.unit,
       show: item.visible,
@@ -14,12 +18,18 @@ export const plitaosb3ru = (inputArr) => {
       imgs: item.images,
       paths: item.path,
     }))
-
     .sort((a, b) => {
       return a.title === b.title ? 0 : a.title < b.title ? -1 : 1;
     })
     .reduce((pre, cur, i) => {
-      if (cur.title === pre.title || cur.title === pre.reverse()[0]?.title) {
+      let preTitle = null;
+      if (pre.title) {
+        preTitle = pre.title;
+      }
+      if (pre[0]?.title) {
+        preTitle = pre[0]?.title;
+      }
+      if (cur.title === preTitle) {
         if (i === inputArr.length - 1) {
           arr.push(Array.prototype.concat(cur, pre));
           return arr;
@@ -27,38 +37,45 @@ export const plitaosb3ru = (inputArr) => {
           return Array.prototype.concat(cur, pre);
         }
       } else {
-        arr.push(pre);
+        if (pre[0]) {
+          arr.push(pre);
+        } else {
+          arr.push([pre]);
+        }
         return cur;
       }
     })
     .map((item) =>
-      item.reduce(
-        (pre, cur) => {
-          pre.infos.push(cur.infos);
-          pre.prices.push(cur.prices);
-          pre.priceFor.push(cur.priceFor);
-          pre.show.push(cur.show);
-          pre.articles.push(cur.articles);
-          pre.ids.push(cur.ids);
-          pre.coef.push(cur.coef);
-          pre.imgs.push(cur.imgs);
-          pre.paths.push(cur.paths);
-          return pre;
-        },
-        {
-          title: item[0].title,
-          // sizes: [],
-          infos: [],
-          prices: [],
-          priceFor: [],
-          show: [],
-          articles: [],
-          ids: [],
-          coef: [],
-          imgs: [],
-          paths: [],
-        }
-      )
+      item
+        // .sort((a, b) => a.sizes.h - b.sizes.h || a.sizes.a - b.sizes.a || a.sizes.b - b.sizes.b)
+        .reduce(
+          (pre, cur) => {
+            // pre.sizes.push(cur.sizes);
+            pre.infos.push(cur.infos)
+            pre.prices.push(cur.prices);
+            pre.priceFor.push(cur.priceFor);
+            pre.show.push(cur.show);
+            pre.articles.push(cur.articles);
+            pre.ids.push(cur.ids);
+            pre.coef.push(cur.coef);
+            pre.imgs.push(cur.imgs);
+            pre.paths.push(cur.paths);
+            return pre;
+          },
+          {
+            title: item[0].title,
+            // sizes: [],
+            infos: [],
+            prices: [],
+            priceFor: [],
+            show: [],
+            articles: [],
+            ids: [],
+            coef: [],
+            imgs: [],
+            paths: [],
+          }
+        )
     );
   // return inputArr.map((inputArrItem) => {
   //   let tmpSizes = inputArrItem.sizes.map((tmpSizesItem) => {
@@ -77,7 +94,6 @@ export const plitaosb3ru = (inputArr) => {
   //   return inputArrItem;
   // });
 };
-
 export const belplit24ru = (inputArr) => {
   // console.log("🚀 ~ file: functions.js ~ line 83 ~ belplit24ru ~ inputArr", inputArr)
   let arr = [];
