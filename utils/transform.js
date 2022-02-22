80b83e47cfd0cd3ec279a64c20ff021910b6c95e
sds92 <safronovds92@yandex.ru>
2022-02-22 10:22:56 +0300

    stable

--- utils/transform.js
+++ utils/transform.js
@@ -1,16 +1,19 @@
 export const transform = (input) => {
-// console.log("🚀 ~ file: transform.js ~ line 2 ~ transform ~ input", input)
+  // console.log("🚀 ~ file: transform.js ~ line 2 ~ transform ~ input", input)
   let res = [];
   // here we should transform all options to obj entries
   // sort by title
   // sort by sizes or if no sizes - by price
   // group by product
   let temp = [];
-  input
+  let categories = input
     .map((item) => {
       // specifying category
       // trying different options
       let category = '';
+      let subcategory = '';
+      let sizes = {};
+      let tempSizes = '';
       // if (item.options.find(({ key }) => key === 'Производитель')) {
       //   category = item.options.find(({ key }) => key === 'Производитель').value;
       // }
@@ -25,41 +28,55 @@ export const transform = (input) => {
       // }
       switch (process.env.NEXT_PUBLIC_SITE_URL) {
         case 'pilomateriali.site':
-          item.title.includes('Брус стро')
-        ? category = 'Брус стороганый'
-        : item.title.includes('Евровагонка из хвои')
-        ? category = 'Евровагонка из хвои'
-        : category = 'aaa'
+          // set category
+          /Брус стро/.test(item.title)
+            ? (category = 'Брус строганный')
+            : /Евровагонка из хвои/.test(item.title)
+            ? (category = 'Евровагонка')
+            : /Имитация бруса/.test(item.title)
+            ? (category = 'Имитация бруса')
+            : /Панель штиль|Панель Штиль/.test(item.title)
+            ? (category = 'Панель штиль')
+            : /Половая доска/.test(item.title)
+            ? (category = 'Половая доска')
+            : (category = 'Без категории');
+          // set subcategory
+          if (item.options.find(({ key }) => key === 'Сорт')) {
+            subcategory = item.options.find(({ key }) => key === 'Сорт').value;
+          }
+          // set sizes
+          sizes.a = parseInt(item.options.find(({ key }) => key === 'Длина')?.value.replace('мм', ''));
+          sizes.b = parseInt(item.options.find(({ key }) => key === 'Ширина')?.value.replace('мм', ''));
+          sizes.h = parseInt(item.options.find(({ key }) => key === 'Толщина')?.value.replace('мм', ''));
           break;
-      
+
         default:
           break;
       }
       // specifying sizes
       // 1st - try from options
       // 2nd - try from title
-      let sizes = {};
-      let tempSizes = '';
 
-      tempSizes = item.title
-        .match(/([0-9]+[^0-9][0-9]+[^0-9][0-9]+)/)?.[0]
-        .split(/[^0-9]/)
-        .map((a) => parseInt(a))
-        .sort((a, b) => a - b)
-        .reverse();
+      // tempSizes = item.title
+      //   .match(/([0-9]+[^0-9][0-9]+[^0-9][0-9]+)/)?.[0]
+      //   .split(/[^0-9]/)
+      //   .map((a) => parseInt(a))
+      //   .sort((a, b) => a - b)
+      //   .reverse();
 
-      if (tempSizes) {
-        sizes.a = tempSizes[0];
-        sizes.b = tempSizes[1];
-        sizes.h = tempSizes[2];
-      } else {
-        sizes.a = parseInt(item.options.find(({ key }) => key === 'Длина')?.value.replace('мм', ''));
-        sizes.b = parseInt(item.options.find(({ key }) => key === 'Ширина')?.value.replace('мм', ''));
-        sizes.h = parseInt(item.options.find(({ key }) => key === 'Толщина')?.value.replace('мм', ''));
-      }
-      sizes.m = parseInt(item.options.find(({ key }) => key === 'Вес')?.value.replace(/кг|мг/, ''));
+      // if (tempSizes) {
+      //   sizes.a = tempSizes[0];
+      //   sizes.b = tempSizes[1];
+      //   sizes.h = tempSizes[2];
+      // } else {
+      //   sizes.a = parseInt(item.options.find(({ key }) => key === 'Длина')?.value.replace('мм', ''));
+      //   sizes.b = parseInt(item.options.find(({ key }) => key === 'Ширина')?.value.replace('мм', ''));
+      //   sizes.h = parseInt(item.options.find(({ key }) => key === 'Толщина')?.value.replace('мм', ''));
+      // }
+      // sizes.m = parseInt(item.options.find(({ key }) => key === 'Вес')?.value.replace(/кг|мг/, ''));
 
       return {
+        subcategory: subcategory,
         category: category,
         title: item.title,
         price: item.cost,
@@ -77,27 +94,34 @@ export const transform = (input) => {
       return a.category === b.category ? 0 : a.category < b.category ? -1 : 1;
     })
     .reduce((pre, cur, i) => {
+      if (i === input.length - 1) {
+        // console.log("🚀 ~ file: functions.js ~ line 31 ~ .reduce ~ arr", arr)
+        res.push(Array.prototype.concat(cur, pre));
+        return res;
+      }
       let preCategory = null;
-      // console.log("🚀 ~ pre",i, pre)
-
       if (pre.category) {
         preCategory = pre.category;
-      } else if (pre[0].category) {
+      }
+      if (pre[0]?.category) {
         preCategory = pre[0]?.category;
       }
-      console.log("🚀 ~ preCategory",i, preCategory)
-      if (preCategory === cur.category) {
-        if (i === input.length - 1) {
-          return res.push(Array.prototype.concat(cur, pre));
+      if (cur.category === preCategory) {
+        if (i === input.length + 1) {
+          res.push(Array.prototype.concat(cur, pre));
+          return res;
+        } else {
+          return Array.prototype.concat(cur, pre);
         }
-        return Array.prototype.concat(cur, pre);
       } else {
-        res.push([cur]);
-        // return cur
+        if (pre[0]) {
+          res.push(pre);
+        } else {
+          res.push([pre]);
+        }
+        return cur;
       }
-      res.push(pre.sort((a, b) => a.sizes.h - b.sizes.h || a.sizes.a - b.sizes.a || a.sizes.b - b.sizes.b));
-      return cur;
     });
-    // res = temp
-    return res
+  // res = temp
+  return res;
 };
