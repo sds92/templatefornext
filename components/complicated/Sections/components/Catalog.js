ea74b1425de534172ad739d75568136210eb80d7
sds92 <safronovds92@yandex.ru>
2022-03-01 16:30:42 +0300

    stable

--- components/complicated/Sections/components/Catalog.js
+++ components/complicated/Sections/components/Catalog.js
@@ -75,7 +75,7 @@ export default function Catalog(props) {
                           >
                             {/* SUBCATEGORIES */}
                             {item.items
-                              .sort((a, b) => (a === 'Другие' || a-b ? 1 : -1))
+                              .sort((a, b) => (a === 'Другие' | a-b ? 1 : -1))
                               .map((item_i, index_i) => {
                                 return (
                                   <div
