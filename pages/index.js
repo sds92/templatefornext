e67d0eb5d3e1fe19b4668e9e4b9044ab8cbc2ef3
sds92 <safronovds92@yandex.ru>
2022-03-02 15:01:20 +0300

    stable

--- pages/index.js
+++ pages/index.js
@@ -5,7 +5,13 @@ import { animations } from '../styles/animations';
 import { transform } from '../utils/transform';
 
 export default function Home(props) {
-  // console.log('🚀 ~ file: index.js ~ line 8 ~ Home ~ props', props);
+  // console.log('🚀 ~ file: index.js ~ line 8 ~ Home ~ props', props.resContacts);
+  // const newProps = props
+  // newProps.data.contacts.emails = 
+  // if (props.resContacts) {
+  //   newProps.data.contacts = {...newProps.data.contacts, ...props.resContacts};
+  // }
+  // console.log("🚀 ~ file: index.js ~ line 14 ~ Home ~ newProps", newProps)
   return (
     <>
       <Header {...props} />
@@ -30,20 +36,26 @@ export async function getStaticProps(context) {
   const resProducts = await fetch(
     `https://xn--j1ano.com/uploads/staticsites/${encodeURI(process.env.NEXT_PUBLIC_SITE_URL)}.json`
   )
-  .then((res) => res.json())
-  .catch((err) => {return null});
-  
+    .then((res) => res.json())
+    .catch((err) => {
+      return null;
+    });
+
   const resContacts = await fetch(
     `https://xn--j1ano.com/uploads/staticsites/contacts_${encodeURI(process.env.NEXT_PUBLIC_SITE_URL)}.json`
   )
-  .then((res) => res.json())
-  .catch((err) => {return null});
-  
+    .then((res) => res.json())
+    .catch((err) => {
+      return null;
+    });
+
   const resMainBanner = await fetch(
     `https://xn--j1ano.com/uploads/staticsites/mainBanner_${encodeURI(process.env.NEXT_PUBLIC_SITE_URL)}.json`
   )
     .then((res) => res.json())
-    .catch((err) => {return null});
+    .catch((err) => {
+      return null;
+    });
 
   const datafromDB = transform(resProducts.filter((item) => item.visible));
   // By returning { props: { posts } }, the Blog component
