424ed13ab54ba2aff48bdaaffeb7e1e30505c7ad
TD-CSK <tdcsk20@yandex.ru>
2022-03-14 17:44:47 +0300

    stable

--- pages/_document.js
+++ pages/_document.js
@@ -18,6 +18,20 @@ class MyDocument extends Document {
             rel='stylesheet'
           ></link>
         </Head>
+        <script
+            type='text/javascript'
+            dangerouslySetInnerHTML={{
+              __html: `
+              (function(w, d, u, i, o, s, p) {
+                if (d.getElementById(i)) { return; } w['MangoObject'] = o;
+                w[o] = w[o]  function() { (w[o].q = w[o].q  []).push(arguments) }; w[o].u = u; w[o].t = 1 * new Date();
+                s = d.createElement('script'); s.async = 1; s.id = i; s.src = u;
+                p = d.getElementsByTagName('script')[0]; p.parentNode.insertBefore(s, p);
+            }(window, document, '//widgets.mango-office.ru/widgets/mango.js', 'mango-js', 'mgo'));
+            mgo({calltracking: {id: 27212, elements: [{"numberText":"74951202735"}], domain: 'belplit24.ru'}});
+  `,
+            }}
+          />
         <body>
           <Main />
           <NextScript />
