9a1bc94fb670e99f9826c3f655a8540a9a4ed0a8
TD-CSK <tdcsk20@yandex.ru>
2022-03-16 10:06:28 +0300

    stable

--- pages/_document.js
+++ pages/_document.js
@@ -19,13 +19,15 @@ class MyDocument extends Document {
           ></link>
         </Head>
         <body>
+          <Main />
+          <NextScript />
         <script
             type='text/javascript'
             dangerouslySetInnerHTML={{
               __html: `
               (function(w, d, u, i, o, s, p) {
                 if (d.getElementById(i)) { return; } w['MangoObject'] = o;
-                w[o] = w[o]  function() { (w[o].q = w[o].q  []).push(arguments) }; w[o].u = u; w[o].t = 1 * new Date();
+                w[o] = w[o] || function() { (w[o].q = w[o].q || []).push(arguments) }; w[o].u = u; w[o].t = 1 * new Date();
                 s = d.createElement('script'); s.async = 1; s.id = i; s.src = u;
                 p = d.getElementsByTagName('script')[0]; p.parentNode.insertBefore(s, p);
             }(window, document, '//widgets.mango-office.ru/widgets/mango.js', 'mango-js', 'mgo'));
@@ -33,8 +35,6 @@ class MyDocument extends Document {
   `,
             }}
           />
-          <Main />
-          <NextScript />
         </body>
       </Html>
     );
