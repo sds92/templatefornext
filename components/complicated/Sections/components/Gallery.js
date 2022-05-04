68495c7a8954708131356cce9bbb3ebadc2eb97b
Dmitry <safronovds92@yandex.ru>
2022-05-04 18:11:23 +0300

    stable

--- components/complicated/Sections/components/Gallery.js
+++ components/complicated/Sections/components/Gallery.js
@@ -2,7 +2,6 @@ import React from 'react';
 import { Title, SubTitle } from '../../../lib';
 
 export default function Gallery(props) {
-  const { w, lgView, app, theme, products } = props;
   const content = props.content.content.gallery;
   const imgs = content[2];
   const [state, setState] = React.useState({
@@ -19,7 +18,7 @@ export default function Gallery(props) {
           <>
             <div className={`flex flex-wrap justify-center items-center`}>
               {imgs.map((item, index) => (
-                <div key={`IMG${index}`} className={`cursor-pointer overflow-hidden basis-1/2`}>
+                <div key={`IMG${index}`} className={`cursor-pointer overflow-hidden md:basis-1/2 p-4`}>
                   <div
                     className={`relative`}
                     onMouseEnter={() =>
@@ -33,30 +32,38 @@ export default function Gallery(props) {
                       })
                     }
                   >
-                    <img
-                      className={`transition-all duration-1000 ${
-                        state.hover === index
-                          ? 'scale-110'
-                          : ''
-                      }`}
+                    <div
+                      className={`relative h-full w-full transition-all duration-1000 overflow-hidden `}
+                    >
+                      <img
+                        alt=''
+                        className={`min-h-full transition-all duration-1000 ${
+                          state.hover === index ? 'scale-110' : ''
+                        } w-auto object-cover`}
+                        src={`${item[0]}.webp`}
+                      >
+                      </img>
+                        <div
+                          className={`absolute shadow-2xl inset-0 bg-black bg-opacity-30 md:bg-opacity-0 hover:bg-opacity-30 duration-500 transition-all overflow-hidden`}
+                        >
+                          <p
+                            className={`uppercase bg-belplit24_2 absolute bottom-0 mb-4 text-left text-slate-100 leading-5 transition-all px-2 py-1  ${
+                              state.hover === index
+                                ? 'bg-opacity-80 opacity-100 translate-x-'
+                                : 'bg-opacity-0 opacity-0 -translate-x-10'
+                            }`}
+                          >
+                            {item[1]}
+                          </p>
+                        </div>
+                    </div>
+                    {/* <img
+                      className={`transition-all duration-1000 ${state.hover === index ? 'scale-110' : ''}`}
                       src={`${item[0]}.webp`}
                       alt=''
                       width='370'
                       height='256'
-                    />
-                    <div
-                      className={`absolute inset-0 bg-black bg-opacity-30 md:bg-opacity-0 hover:bg-opacity-30 duration-500 transition-all overflow-hidden`}
-                    >
-                      <p
-                        className={`uppercase bg-belplit24_2 absolute bottom-0 mb-4 text-left text-slate-100 leading-5 transition-all px-2 py-1  ${
-                          state.hover === index
-                            ? 'bg-opacity-80 opacity-100 translate-x-'
-                            : 'bg-opacity-0 opacity-0 -translate-x-10'
-                        }`}
-                      >
-                        {item[1]}
-                      </p>
-                    </div>
+                    /> */}
                   </div>
                 </div>
               ))}
