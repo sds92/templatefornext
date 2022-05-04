423070e9938d0d215de9daccbb2e60887838ebcb
Dmitry <safronovds92@yandex.ru>
2022-05-04 10:59:57 +0300

    stable

--- components/complicated/Sections/components/Gallery.js
+++ components/complicated/Sections/components/Gallery.js
@@ -3,6 +3,9 @@ import { Title, SubTitle } from '../../../lib';
 
 export default function Gallery({ content }) {
   const imgs = content[2]
+  const [state, setState] = React.useState({
+    hover: null,
+  });
   return (
     <div className={`py-20`}>
       <Title a={content[0][0]} b={content[0][1]} />
@@ -10,22 +13,48 @@ export default function Gallery({ content }) {
       <hr />
       <br />
       <div className={`flex gap-4 flex-wrap justify-center md:w-10/12 lg:w-8/12 mx-auto`}>
-        {imgs.map((item, index) => (
-          <div key={`IMG${index}`} className={`flex-none`}>
-            <div className={`relative`}>
-              <img className={``} src={`${item[0]}.jpg`} alt width='370' height='256' />
-              <div
-                className={`absolute inset-0 bg-black bg-opacity-30 md:bg-opacity-0 hover:bg-opacity-30 duration-500 transition-all`}
-              >
-                <p
-                  className={`uppercase absolute inset-0 user-pt-1/2 text-center text-slate-100 md:opacity-0 hover:opacity-100 transition-all`}
+      {imgs.length !== 0 && (
+        <>
+          <div className={`flex gap-4 flex-wrap justify-center md:w-10/12 lg:w-8/12 mx-auto`}>
+            {imgs.map((item, index) => (
+              <div key={`IMG${index}`} className={``}>
+                <div
+                  className={`relative`}
+                  onMouseEnter={() =>
+                    setState((state) => {
+                      return { ...state, hover: index };
+                    })
+                  }
+                  onMouseLeave={() =>
+                    setState((state) => {
+                      return { ...state, hover: null };
+                    })
+                  }
                 >
-                  {item[1]}
-                </p>
+                  <img className={``} src={`images/${data.url}/${item[0]}`} alt width='370' height='256' />
+                  <div
+                    className={`absolute inset-0 bg-black bg-opacity-30 md:bg-opacity-0 hover:bg-opacity-30 duration-500 transition-all overflow-hidden`}
+                  >
+                    <p
+                      className={`uppercase absolute bottom-0 mb-4 text-left text-slate-100 leading-5 transition-all px-2 py-1 ${
+                        styles.imgTitle
+                      } bg-${theme.bg.productcardPrice} ${
+                        state.hover === index
+                          ? 'bg-opacity-80 opacity-100 translate-x-0'
+                          : 'bg-opacity-0 opacity-0 -translate-x-10'
+                      }`}
+                    >
+                      {item[1]}
+                    </p>
+                  </div>
+                </div>
               </div>
-            </div>
+            ))}
           </div>
-        ))}
+          <br />
+          <hr />
+        </>
+      )}
       </div>
       <br />
       <hr />
