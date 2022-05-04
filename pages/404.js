f841dfdfcadbccb33a4a542ac2210337af027aa3
Dmitry <safronovds92@yandex.ru>
2022-05-04 14:58:22 +0300

    stable

---
+++ pages/404.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import { useRouter } from 'next/router';
+import { Header, Footer } from '../components/complicated';
+
+export default function Home({lgView, app, menu, theme, products }) {
+  return (
+    <body>
+      <>
+        <Header lgView={lgView} menu={menu} app={app} theme={theme} />
+        <div className='h-16'></div>
+        {/* <FullPage/> */}
+        <div>Страница не найдена</div>
+        <Footer app={app} />
+      </>
+    </body>
+  );
+}
