b7a099f5f7575f0651c8fb7e79c5f14750a6df36
Dmitry <safronovds92@yandex.ru>
2022-05-05 09:39:27 +0300

    stable

--- components/complicated/Sections/components/Catalog.js
+++ components/complicated/Sections/components/Catalog.js
@@ -149,7 +149,7 @@ export default function Catalog({ w, lgView, content, app, theme, products }) {
                         >
                           <img
                             className={`${state.hover === index && `scale-105`} duration-1000 transition-all`}
-                            src={'/images/shinglas/products.webp'}
+                            src={'/images/osb/product.webp'}
                             alt=''
                             width='370'
                             height='256'
