d5f2b03245d0bb87b17dca19eb9f963100aa160b
Dmitry <safronovds92@yandex.ru>
2022-05-12 12:44:17 +0300

    stable

---
+++ pages/500.js
@@ -0,0 +1,28 @@
+import React from 'react';
+import fs from 'fs';
+import Link from 'next/link';
+import { Logos } from '../components/complicated';
+
+export default function Custom500(props) {
+  const Logo = Logos[app?.logo] || null;
+  return (
+    <div className={`w-screen h-screen flex flex-col justify-center items-center`}>
+      <Link href='/' passHref>
+        <Logo />
+      </Link>
+      <div className={`font-light text-zinc-800`}>Внутренняя ошибка сервера</div>
+      <div className={`font-light text-zinc-800`}>Internal server error</div>
+    </div>
+  );
+}
+
+export async function getStaticProps() {
+  let app = JSON.parse(fs.readFileSync('data/app.ru.json', 'utf8'));
+
+  return {
+    props: {
+      app,
+    },
+    revalidate: 5,
+  };
+}
