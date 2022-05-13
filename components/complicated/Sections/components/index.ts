142c18b3e6763b387d4f6d9e1f8614269b2839d3
Dmitry <safronovds92@yandex.ru>
2022-05-13 10:30:08 +0300

    stable

--- components/complicated/Sections/components/index.ts
+++ components/complicated/Sections/components/index.ts
@@ -1,4 +1,3 @@
-export { default as PromoBlock } from './PromoBlock';
 export { default as CardsGrid } from './CardsGrid';
 export { default as Slider } from './Slider';
 export { default as SectionTitle } from './SectionTitle';
