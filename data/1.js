f3877e05fcc1a4be2dae869d29da6c46f0c1c09d
Dmitry <safronovds92@yandex.ru>
2022-05-26 11:16:42 +0300

    stable

---
+++ data/1.js
@@ -0,0 +1,37 @@
+
+"Атлантика" : ["60 лет", "3x", "reliability", "loudness", "sbs2"]
+"Вестерн" : ["55 лет", "reliability", "loudness", "sbs2", "2x"]
+"Джаз" : ["50 лет", "reliability", "loudness", "sbs2", "2x" ]
+"Кантри" : ["50 лет", "reliability", "loudness", "sbs2", "2x" ]
+"Континент" : ["60 лет", "3x", "reliability", "loudness", "sbs2"]
+"Самба" : ["50 лет", "infinity", "snowProtection", "sbs", "1x"]
+"Танго" : ["30 лет", "infinity", "snowProtection", "sbs", "1x"]
+"Аккорд" : ["20 лет", "infinity", "snowProtection", "sbs", "1x"]
+"Соната" : ["20 лет", "infinity", "snowProtection", "sbs", "1x"]         
+"Фламенко" : ["30 лет", "infinity", "snowProtection", "sbs", "1x"]   
+"Фокстрот" : ["50 лет", "infinity", "snowProtection", "sbs", "1x"]          
+"Модерн" : ["30 лет", "infinity", "snowProtection", "sbs", "1x"]        
+"Сальса" : ["30 лет", "infinity", "snowProtection", "sbs", "1x"]
+"Кадриль" : ["30 лет", "infinity", "snowProtection", "sbs", "1x"]         
+"Ранчо" : ["50 лет", "reliability", "loudness", "sbs2", "2x" ] 
+"Фазенда" : ["20 лет", "loudness", "sbs2", "2x", "easeofuse" ]
+"Оптима" : ["10 лет", "loudness", "easeofuse", "sbs2", "1x"]    
+"Болеро" : ["30 лет", "infinity", "snowProtection", "sbs", "1x"]
+                
+let a = {
+    infinity: "ДОЛГОВЕЧНОСТЬ",
+
+
+}
+                
+
+                
+              
+
+                
+              
+
+                
+
+
+              
